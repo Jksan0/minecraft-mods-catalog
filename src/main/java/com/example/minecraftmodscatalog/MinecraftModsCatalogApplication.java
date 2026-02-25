b15bb8d7c58f6c9972f19a3d1793bed1e2e4a6d2
Jksan0 <kurilinevgenij5@gmail.com>
2026-02-26 02:22:17 +0300

    №1

---
+++ src/main/java/com/example/minecraftmodscatalog/MinecraftModsCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.example.minecraftmodscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinecraftModsCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinecraftModsCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/minecraftmodscatalog/MinecraftModsCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minecraftmodscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinecraftModsCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

91ac4d75d583504df5acefe4a524143b49b99b55
Jksan0 <kurilinevgenij5@gmail.com>
2026-03-04 13:23:23 +0300

    №11

--- src/main/java/com/example/minecraftmodscatalog/MinecraftModsCatalogApplication.java
+++ src/main/java/com/example/minecraftmodscatalog/MinecraftModsCatalogApplication.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MinecraftModsCatalogApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MinecraftModsCatalogApplication.class, args);
     }

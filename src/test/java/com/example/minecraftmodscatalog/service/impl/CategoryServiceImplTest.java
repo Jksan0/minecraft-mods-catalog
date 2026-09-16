649b221e69484319ef5bdc1ce286aadee89dbe25
Jksan0 <kurilinevgenij5@gmail.com>
2026-09-16 14:40:54 +0300

    №24

--- src/test/java/com/example/minecraftmodscatalog/service/impl/CategoryServiceImplTest.java
+++ src/test/java/com/example/minecraftmodscatalog/service/impl/CategoryServiceImplTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
--- src/test/java/com/example/minecraftmodscatalog/service/impl/ModServiceImplTest.java
+++ src/test/java/com/example/minecraftmodscatalog/service/impl/ModServiceImplTest.java
@@ -20,7 +20,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
